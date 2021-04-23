95d4bd147022622d75da62553ecb1bbfdecf36c3
rajdeep345 <rajdeep345.mukherjee@yahoo.com>
2021-04-23 21:44:42 +0530

    Removed old scripts

--- run_script1.sh
+++
@@ -1,5 +0,0 @@
-python3 ptrnet_bert.py 0 42 lap14/ lap14/ptrnet_bert_run5 train gen 16 30
-python3 ptrnet_bert.py 0 42 14res/ 14res/ptrnet_bert_run5 train gen 16 30
-python3 ptrnet_bert.py 0 42 15res/ 15res/ptrnet_bert_run5 train gen 16 30
-python3 ptrnet_bert.py 0 42 16res/ 16res/ptrnet_bert_run5 train gen 16 30
-python3 ptrnet_bert.py 0 42 resall/ resall/ptrnet_bert_run5 train gen 16 50
