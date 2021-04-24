88c4842c116efa39c3fba71c44a2668924d2e09c
rajdeep345 <rajdeep345.mukherjee@yahoo.com>
2021-04-25 03:16:05 +0530

    Updated ptrnet_bert.py

--- run_script_lap.sh
+++ run_script_lap.sh
@@ -1,49 +1,49 @@
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct af
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct af --l2 y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct af --l2 y --wd 1e-5
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct af --optim adamw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct af
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct af --l2 y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct af --l2 y --wd 1e-5
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct af --optim adamw
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --l2 y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --l2 y --wd 1e-5
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --optim adamw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --l2 y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --l2 y --wd 1e-5
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --optim adamw
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --use_sort n
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --l2 y --use_sort n
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --l2 y --wd 1e-5 --use_sort n
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct of --optim adamw --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --l2 y --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --l2 y --wd 1e-5 --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct of --optim adamw --use_sort n
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --l2 y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --l2 y --wd 1e-5
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --optim adamw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --l2 y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --l2 y --wd 1e-5
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --optim adamw
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --use_maxPool y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --l2 y --use_maxPool y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --l2 y --wd 1e-5 --use_maxPool y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode gen --gen_direct bw --optim adamw --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --l2 y --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --l2 y --wd 1e-5 --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode gen --gen_direct bw --optim adamw --use_maxPool y
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct af
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct af --l2 y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct af --l2 y --wd 1e-5
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct af --optim adamw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct af
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct af --l2 y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct af --l2 y --wd 1e-5
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct af --optim adamw
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --l2 y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --l2 y --wd 1e-5
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --optim adamw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --l2 y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --l2 y --wd 1e-5
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --optim adamw
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --use_sort n
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --l2 y --use_sort n
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --l2 y --wd 1e-5 --use_sort n
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct of --optim adamw --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --l2 y --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --l2 y --wd 1e-5 --use_sort n
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct of --optim adamw --use_sort n
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --l2 y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --l2 y --wd 1e-5
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --optim adamw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --l2 y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --l2 y --wd 1e-5
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --optim adamw
 
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --use_maxPool y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --l2 y --use_maxPool y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --l2 y --wd 1e-5 --use_maxPool y
-python3 ptrnet_bert.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/ptrnet_bert --bert_mode lap --gen_direct bw --optim adamw --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --l2 y --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --l2 y --wd 1e-5 --use_maxPool y
+python3 ptrnet_bert2.py --gpu_id 0 --src_folder lap14/ --trg_folder lap14/v2_ptrnet_bert --bert_mode lap --gen_direct bw --optim adamw --use_maxPool y
