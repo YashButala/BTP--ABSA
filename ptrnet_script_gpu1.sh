720f4aa6a8a1bf11047a39103766f8f4ff891259
rajdeep345 <rajdeep345.mukherjee@yahoo.com>
2021-05-05 17:58:57 +0530

    Updated ptrnet_decoder.py and added scripts

--- ptrnet_script_gpu1.sh
+++ ptrnet_script_gpu1.sh
@@ -1,13 +1,13 @@
-python3 ptrnetwork_bert.py 1 1024 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1024 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1024 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1024 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1024 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1024 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1024 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1024 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1024 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1024 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
 
-python3 ptrnetwork_bert.py 1 1026 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1026 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1026 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1026 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnetwork_bert.py 1 1026 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1026 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1026 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1026 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1026 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 1 1026 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
 
-python3 ptrnetwork_bert.py 0 1027 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder.py 0 1027 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
