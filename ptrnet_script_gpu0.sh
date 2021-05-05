b7f6a5b03882851c4dea4ae6b1339465101d5c75
rajdeep345 <rajdeep345.mukherjee@yahoo.com>
2021-05-05 17:49:36 +0530

    Updated ptrnet_decoder.py and added scripts

---
+++ ptrnet_script_gpu0.sh
@@ -0,0 +1,16 @@
+python3 ptrnetwork_bert.py 0 1023 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1023 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1023 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1023 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1023 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+
+python3 ptrnetwork_bert.py 0 1025 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1025 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1025 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1025 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1025 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+
+python3 ptrnetwork_bert.py 0 1027 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1027 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1027 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnetwork_bert.py 0 1027 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
