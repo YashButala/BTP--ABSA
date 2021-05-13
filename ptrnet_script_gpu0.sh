e1806824370daa5decc7080323920c3ab84ea74c
rajdeep345 <rajdeep345.mukherjee@yahoo.com>
2021-05-13 17:13:08 +0530

    Updated scripts for ptrnet_decoder

--- ptrnet_script_gpu0.sh
+++ ptrnet_script_gpu0.sh
@@ -1,16 +1,16 @@
-python3 ptrnet_decoder.py 0 1023 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1023 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1023 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1023 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1023 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1023 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1023 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1023 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1023 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1023 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
 
-python3 ptrnet_decoder.py 0 1025 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1025 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1025 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1025 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1025 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1025 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1025 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1025 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1025 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1025 resall/ resall/ptrnet_decoder train 10 100 100 AF AP
 
-python3 ptrnet_decoder.py 0 1027 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1027 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1027 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
-python3 ptrnet_decoder.py 0 1027 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1027 lap14/ lap14/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1027 14res/ 14res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1027 15res/ 15res/ptrnet_decoder train 10 100 100 AF AP
+python3 ptrnet_decoder_DEP.py 0 1027 16res/ 16res/ptrnet_decoder train 10 100 100 AF AP
